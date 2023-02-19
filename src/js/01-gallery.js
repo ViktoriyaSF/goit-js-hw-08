c139c97dd4e000a483d7271b3a587fa72b32908d
ViktoriyaSF <kievstar01051985@gmail.com>
2023-02-19 19:41:32 +0200

    task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,31 @@
+// Описаний в документації
+import SimpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+// змінні по роботі
+const galleryEl = document.querySelector('.gallery');
+
+// сформування розмітки з класами для  html
+const galleryImg = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<a class="gallery__item" href="${original}" ><img class="gallery__image" src="${preview}" alt="Image ${description}"/></a>`
+  )
+  .join('');
+
+galleryEl.insertAdjacentHTML('beforeend', galleryImg);
+
+// створення виклику
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  close: false,
+  showCounter: false,
+});
