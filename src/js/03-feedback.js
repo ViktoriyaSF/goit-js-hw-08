3d8eb5463a2d6a28cc819b9b662f4aa204a1d636
ViktoriyaSF <kievstar01051985@gmail.com>
2023-02-23 22:34:45 +0200

    task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,24 @@
+import throttle from 'lodash.throttle';
 
+const refs = {
+  form: document.querySelector('form'),
+  email: document.querySelector('input'),
+  message: document.querySelector('textarea'),
+};
+
+refs.form.addEventListener('input', throttle(formInput, 500));
+refs.form.addEventListener('submit', formOnSubmit);
+
+function formInput() {
+  const allInput = {
+    email: refs.email.value.trim(),
+    message: refs.message.value.trim(),
+  };
+  localStorage.setItem('feedback-form-state', JSON.stringify(allInput));
+}
+
+function formOnSubmit(event) {
+  event.preventDafualt();
+  refs.form.resert();
+  localStorage.removeItem('feedback-form-state');
+}
